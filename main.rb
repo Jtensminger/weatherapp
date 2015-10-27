28252d41999e6507c98dd4265adaf1501059a620
Joseph Ensminger <Jtensminger@hotmail.com>
2015-10-26 22:36:59 -0500

    Added main.rb for utilizing classes

--- main.rb
+++ main.rb
@@ -2,19 +2,15 @@ require './sun'
 require './hurricane'
 require './sevenday'
 require './alerts'
-times = Sun.new(37814)
+
+puts "Please enter 5 digit zipcode to receive local weather report and storm warnings."
+zipcode = gets.chomp.to_i
+times = Sun.new(zipcode)
 hurricanes = Hurricane.new
-sevenday_forecast = SevenDayForecast.new(37814)
-alerts = Alerts.new(37814)
+sevenday_forecast = SevenDayForecast.new(zipcode)
+alerts = Alerts.new(zipcode)
 
 puts times.get_times
 puts sevenday_forecast.get_sevenDay
 puts hurricanes.get_hurricanes
 puts alerts.get_alerts
-# puts "Please enter zip code."
-# user_zipcode = gets.chomp.to_i
-# api_key = 'c44c46161f9c4f8a'
-# url4 = "http://api.wunderground.com/api/#{api_key}/alerts/q/#{user_zipcode}.json"
-# response = HTTParty.get(url)
-# alerts = response4['alerts']
-# puts "Current weather alerts include #{alerts}" unless alerts.empty?
